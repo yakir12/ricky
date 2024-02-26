ca9e2fcee751ed914d8b1a1ae0f7dae82b1b00db
Yakir Gagnon <12.yakir@gmail.com>
2024-02-26 13:03:27 +0100

    wip

--- generate_tags/main.jl
+++ generate_tags/main.jl
@@ -25,13 +25,17 @@ tag_bytes = 8 # due to the family
 ratio = tag_width/tag_bytes*mm2pt
 paths = get_path.(0:29, ratio)
 
-fig = Figure(size=(595, 842), figure_padding = 0)
-ax = Axis(fig[1, 1], aspect = DataAspect(), limits = (0, 595, 0, 842))
-# colors = distinguishable_colors(5, [RGB(0,0,0), RGB(1,1,1)], dropseed=false)[2:end]
-colors = (RGB{N0f8}(1,0,0), RGB{N0f8}(0,1,0), RGB{N0f8}(0,0,1), colorant"white")
+a4 = (595, 842)
+buff = 100
+
+fig = Figure(size=a4, figure_padding = 0)
+ax = Axis(fig[1, 1], aspect = DataAspect(), limits = (0, a4[1], 0, a4[2]))
+poly!(ax, Rect2(buff, buff, (a4 .- 2buff)...), color = :gray90)
+colors = distinguishable_colors(4, [RGB(1,1,1)], dropseed=true)
+# colors = (RGB{N0f8}(1,0,0), RGB{N0f8}(0,1,0), RGB{N0f8}(0,0,1), colorant"white")
 for (i, color) in enumerate(colors), (path, (x, y)) in zip(paths, ((x, y) for x in 1:6, y in 1:5))
-    poly!(ax, Rect2(ratio*((tag_bytes + 2)*x), ratio*((i - 1)*5*(tag_bytes + 2) + (tag_bytes + 2)*y), tag_width*mm2pt, tag_width*mm2pt), color = color)
-    poly!(ax, path + ratio*Point((tag_bytes + 2)*x - 1, (i - 1)*5*(tag_bytes + 2) + (tag_bytes + 2)*y - 1); color = :black)
+    poly!(ax, Rect2(buff + ratio*((tag_bytes + 2)*x), buff + ratio*((i - 1)*5*(tag_bytes + 2) + (tag_bytes + 2)*y), tag_width*mm2pt, tag_width*mm2pt), color = :white)
+    poly!(ax, path + Point(buff, buff) + ratio*Point((tag_bytes + 2)*x - 1, (i - 1)*5*(tag_bytes + 2) + (tag_bytes + 2)*y - 1); color)
 end
 hidedecorations!(ax)
 hidespines!(ax)
