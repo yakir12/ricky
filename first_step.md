e05dab1b6cb43c221aaa7c1f9994021b846a382f
Yakir Gagnon <12.yakir@gmail.com>
2024-02-29 15:14:44 +0100

    wip

---
+++ first_step.md
@@ -0,0 +1,12 @@
+Here's one way to go forward:
+1. We buy 1 RPI camera module 2 or 3.
+2. We test it from the side on the ring.
+3. if it works: 
+    - we proceed to buying at least 8 more, setting around the ring, and hopefully we're done.
+4. if it doesn't work: 
+    - if we want to have them in a grid close ot the arena (pros are that the collection volume is small, dis are that there will be a 3x3 grid right above the bees): we buy 8 more and set it up in a grid
+    - if we don't want to have a grid: we need to buy 9 module HQ RPI cameras, 9 
+5. 
+Let's get at least 9 RPI cameras. We'll setup one at a fixed height, even with a tripod, just to see what it looks like in the best case scenario. Then once we have it setup in an ideal configuration, we can move it to the side, from the ring, diagonally covering an area of the arena. If that proves inadecuaet, then we know we're gonn ahave to live with the grid thing. If it works, then great, we'll set it all up. 
+
+If the grid proves necessary then we can either hav it low, and run control experiments to show that it hopefully doesn't affect them. Or we can simply install all the cameras we need in the ceiling, set them up with optics, 
--- main.jl
+++ main.jl
@@ -1,4 +1,3 @@
-using Pluto, PlutoUI
 using Unitful
 import Unitful: mm, cm, m, °
 
@@ -10,4 +9,26 @@ p = 4
 fov = 2atand(arena/2/distance)°
 resolution = bytes*fov*p/(2atand(tag/2distance)°)
 resolution^2/10^6 # → 294 MB
+
+lenses = OrderedDict("6mmm" => (w=63, h=63),
+                     "12MP 8mm" => (w=57.12, h=42.44),
+                     "5MP 25mm" => (w=14.7, h=11.1),)
+cameras = OrderedDict("v2" => (resolution=(w=3280, h=2464), fov=OrderedDict("fixed" => (w=62.2, h=48.8))),
+                      "v3" => (resolution=(w=4608, h=2592), fov=OrderedDict("fixed" => (w=66, h=41))),
+                      "v3w" => (resolution=(w=4608, h=2592), fov=OrderedDict("fixed" => (w=102, h=67))),
+                      "hq" => (resolution=(w=4056, h=3040), fov=lenses),
+                      "gs" => (resolution=(w=1456, h=1088), fov=lenses))
+
+tag = 3mm
+bytes = 6
+p = 5
+for (name, camera) in cameras, (lens, fovwh) in camera.fov
+    fov = fovwh.w
+    resolution = camera.resolution.w
+    distance = round(typeof(1cm), tag/2tand(bytes*fov*p/2resolution))
+    width = round(typeof(1cm), 2distance*tand(fov/2))
+    height = round(typeof(1cm), 2distance*tand(fovwh.h/2))
+    @show name, lens, distance, width, height
+end
+
 
