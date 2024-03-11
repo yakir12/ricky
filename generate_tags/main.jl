9253d6e7600097e80f44ab204fc6b748b997b17d
Yakir Gagnon <12.yakir@gmail.com>
2024-03-11 14:15:06 +0100

    tag

--- generate_tags/main.jl
+++ generate_tags/main.jl
@@ -10,52 +10,44 @@ function rectcommands(r)
     [MoveTo(mi), LineTo(ma[1], mi[2]), LineTo(ma), LineTo(mi[1], ma[2]), ClosePath()]
 end
 
-function get_path(id)
+function get_path(id, ratio)
     tag = getAprilTagImage(id, AprilTags.tag16h5) # a 8×8 grayscale image of the tag
     mat = tag .== zero(tag[1])
     return BezierPath(reduce(vcat, rectcommands(Rect((i - 1)*ratio, (j - 1)*ratio, ratio, ratio)) for i in axes(mat, 1) for j in axes(mat, 2) if mat[i, j]))
 end
 
-function plot1tag!(ax, id, offset, color)
-    w = tag_width*mm2pt
+function plot1tag!(ax, w, ratio, id, offset, color)
     poly!(ax, Rect2(offset..., w, w), color = :white)
-    path = get_path(id)
+    path = get_path(id, ratio)
     poly!(ax, path + offset; color)
-    # b1 = 0.
-    # b2 = 2
-    # for (s, x) in zip((-1,1), (0, w)), (t, y) in zip((-1,1), (0, w))
-    #     lines!(ax, [offset + p for p in (Point2(x + s * b2, y), Point2(x + s * b1, y))], color = :black)
-    #     lines!(ax, [offset + p for p in (Point2(x, y + t * b2), Point2(x, y + t * b1))], color = :black)
-    # end
 end
 
-
 const mm2pt = 72/25.4 # multiply mm to get points
-const tag_width = 3 # mm
 const tag_bytes = 8 # due to the family
-const ratio = tag_width/tag_bytes*mm2pt
 const a4 = (595, 842)
 
 fig = Figure(size=a4, figure_padding = 0);
-ax = Axis(fig[1, 1], aspect = DataAspect())#, limits = (0, a4[1], 0, a4[2]))
-for x in 1:6, y in 1:5
-    id = LinearIndices
-plot1tag!(ax, 1, Point2(0, 0), :red)
-save("0.pdf", fig; pt_per_unit=1)
+ax = Axis(fig[1, 1], aspect = DataAspect(), limits = (0, a4[1], 0, a4[2]))
+for (offset0, tag_width) in zip((Point2(150, 250), Point2(300, 500)), (3, 3.5)) # mm
+    ratio = tag_width/tag_bytes*mm2pt
+    w = tag_width*mm2pt
+    colors = distinguishable_colors(4, [RGB(1,1,1)], dropseed=true)
+    ids = 0:29
+    # offset0 = Point2(a4...) / 2 - Point2(5w, 6w)
 
-
-
-# poly!(ax, Rect2(buff, buff, (a4 .- 2buff)...), color = :gray90)
-colors = distinguishable_colors(4, [RGB(1,1,1)], dropseed=true)
-# colors = (RGB{N0f8}(1,0,0), RGB{N0f8}(0,1,0), RGB{N0f8}(0,0,1), colorant"white")
-for (i, color) in enumerate(colors), (i, id) in enumerate(0:29)
-
-
-for (i, color) in enumerate(colors), (path, (x, y)) in zip(paths, ((x, y) for x in 1:6, y in 1:5))
-    poly!(ax, Rect2(buff + ratio*((tag_bytes + 2)*x), buff + ratio*((i - 1)*5*(tag_bytes + 2) + (tag_bytes + 2)*y), tag_width*mm2pt, tag_width*mm2pt), color = :white)
-    poly!(ax, path + Point(buff, buff) + ratio*Point((tag_bytes + 2)*x - 1, (i - 1)*5*(tag_bytes + 2) + (tag_bytes + 2)*y - 1); color)
+    xticks = range(start=offset0[1], step=w, length=2*5 + 1)
+    yticks = range(start=offset0[2], step=w, length=2*6 + 1)
+    buff = 20
+    vlines!(ax, xticks; ymin = (yticks[1] - buff)/a4[2], ymax = (yticks[end] + buff)/a4[2], color=:gray, linewidth=0.5)
+    hlines!(ax, yticks; xmin = (xticks[1] - buff)/a4[1], xmax = (xticks[end] + buff)/a4[1], color=:gray, linewidth=0.5)
+    for (ij, color) in enumerate(colors), (xy, id) in enumerate(ids)
+        i, j = Tuple(CartesianIndices((2, 2))[ij])
+        x, y = Tuple(CartesianIndices((5, 6))[xy])
+        offset1 = Point2((i - 1)*5w, (j - 1)*6w)
+        offset2 = Point2((x - 1)*w, (y - 1)*w)
+        plot1tag!(ax, w, ratio, id, offset0 + offset1 + offset2, color)
+    end
+    hidedecorations!(ax)
+    hidespines!(ax)
+    save("tags.pdf", fig; pt_per_unit=1)
 end
-hidedecorations!(ax)
-hidespines!(ax)
-
-save("0.pdf", fig; pt_per_unit=1)
