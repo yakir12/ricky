904db38e2a69e00e73a20aedc4bd1894b4700844
Yakir Gagnon <12.yakir@gmail.com>
2024-04-11 13:17:26 +0200

    lauch

--- server/launcher.sh
+++ server/launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $HOME/ricky/server
-$HOME/julia/julia --project=$HOME/ricky/server/Project.toml --threads=4 $HOME/ricky/server/server.jl &
+julia --project=$HOME/ricky/server/Project.toml --threads=4 $HOME/ricky/server/server.jl &
