e4d9623c3e4bd667903a198f148f5ee8ce4bb947
Yakir Gagnon <12.yakir@gmail.com>
2024-04-19 15:54:13 +0200

    wip

--- client/plot_tracks/main.jl
+++ client/plot_tracks/main.jl
@@ -5,6 +5,12 @@ using Base.Threads
 @enum TagColor black=90 red=0 green=120 blue=240
 const ip = "http://192.168.15.165:8000" # through ethernet
 
+const camera_modes = ((w = 990, h = 1332, fps = 120),
+         (w = 2028, h = 1080, fps = 50),
+         (w = 2028, h = 1520, fps = 40),
+         (w = 4056, h = 3040, fps = 10))
+const mode = camera_modes[1]
+
 function get_state()
     r = HTTP.request("GET", "$ip/bees")
     return JSON3.read(String(r.body))
@@ -12,7 +18,7 @@ end
 
 function set_state!(cache)
     for (here, there) in zip(cache, get_state())
-        append!(here[], there)
+        append!(here[], reverse.(there))
         notify(here)
     end
 end
@@ -22,7 +28,7 @@ function main()
     colors = repeat([Symbol.(instances(TagColor))...], inner=30)
     cache = [Observable(Point2f[]) for _ in 1:120]
     fig = Figure()
-    ax = Axis(fig[1,1], aspect=DataAspect(), limits=(0, 2000, 0, 2000))
+    ax = Axis(fig[1,1], aspect=DataAspect(), limits=(0, mode.w, 0, mode.h))
     for (xy, color) in zip(cache, colors)
         lines!(ax, xy; color)
     end
