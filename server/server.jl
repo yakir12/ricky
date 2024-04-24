778f4b1cce64c4862367ea1989749818b628adba
Yakir Gagnon <12.yakir@gmail.com>
2024-04-24 13:26:28 +0200

    wip

--- server/server.jl
+++ server/server.jl
@@ -9,16 +9,16 @@ const buffer = Matrix{N0f8}(undef, sz)
 
 get_Y, get_tags, task = main();
 
-frame() = binary(collect(vec(rawview(imresize!(buffer, normedview(get_Y()))))))
+# frame() = binary(collect(vec(rawview(imresize!(buffer, normedview(get_Y()))))))
 
 # @get "/frame" frame
 
-@get "/bees" get_tags
+# @get "/bees" get_tags
 
 # @post "/setup" function(req)
 #     set_setup(json(req, Dict))
 #     return "done"
 # end
 
-serve(access_log=nothing, host="0.0.0.0", port=8000)
+# serve(access_log=nothing, host="0.0.0.0", port=8000, async=true)
 
