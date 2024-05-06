3160f5923df4813ccce4bb9487f68e22b1cacc41
Yakir Gagnon <12.yakir@gmail.com>
2024-05-06 13:59:11 +0200

    wip

--- stream/main.jl
+++ stream/main.jl
@@ -1,52 +1,75 @@
 using Oxygen
+using ImageCore
 
-const camera_modes = ((w = 990, h = 1332, fps = 120),
-         (w = 2028, h = 1080, fps = 50),
-         (w = 2028, h = 1520, fps = 40),
-         (w = 4056, h = 3040, fps = 10))
-
-function get_buffer_img(w, h)
-    w2 = 64ceil(Int, w/64) # dimension adjustments to hardware restrictions
-    nb = Int(w2*h*3/2) # total number of bytes per img
-    buff = Vector{UInt8}(undef, nb)
-    ystart = 1
-    yend = w2*h
-    Y = view(reshape(view(buff, ystart:yend), w2, h), 1:w, h:-1:1)
-    w4 = Int(w2/2)
-    h4 = Int(h/2)
-    ustart = yend + 1
-    uend = ustart - 1 + w4*h4
-    u = view(reshape(view(buff, ustart:uend), w4, h4), 1:Int(w/2), h4:-1:1)
-    vstart = uend + 1
-    vend = vstart - 1 + w4*h4
-    v = view(reshape(view(buff, vstart:vend), w4, h4), 1:Int(w/2), h4:-1:1)
-    return buff, Y, u, v
+mutable struct FPS{N}
+    i::Int
+    times::MVector{N, UInt64}
+    FPS{N}() where {N} = new(0, MVector{N, UInt64}(1:N))
 end
+FPS(N::Int) = FPS{N}()
 
-struct Camera
-    buff::Vector{UInt8}
-    Y::SubArray{UInt8, 2, Base.ReshapedArray{UInt8, 2, SubArray{UInt8, 1, Vector{UInt8}, Tuple{UnitRange{Int64}}, true}, Tuple{}}, Tuple{UnitRange{Int64}, StepRange{Int64, Int64}}, false}
-    u::SubArray{UInt8, 2, Base.ReshapedArray{UInt8, 2, SubArray{UInt8, 1, Vector{UInt8}, Tuple{UnitRange{Int64}}, true}, Tuple{}}, Tuple{UnitRange{Int64}, StepRange{Int64, Int64}}, false}
-    v::SubArray{UInt8, 2, Base.ReshapedArray{UInt8, 2, SubArray{UInt8, 1, Vector{UInt8}, Tuple{UnitRange{Int64}}, true}, Tuple{}}, Tuple{UnitRange{Int64}, StepRange{Int64, Int64}}, false}
-    proc::Base.Process
-    function Camera(mode)
-        w, h, fps = mode
-        buff, Y, u, v = get_buffer_img(w, h)
-        proc = open(`rpicam-vid --denoise cdn_off -n --framerate $fps --width $w --height $h --timeout 0 --codec yuv420 -o -`)
-        eof(proc)
-        Threads.@spawn while isopen(proc)
-            read!(proc, buff)
-            yield()
-        end
-        new(buff, Y, u, v, proc)
+function tick!(fps::FPS{N}) where N
+    fps.i += 1
+    fps.times[fps.i] = time_ns()
+    if fps.i == N
+        println(round(Int, 10^9/mean(diff(fps.times))))
+        fps.i = 0
     end
 end
 
-cam = Camera(camera_modes[1])
+include(joinpath(@__DIR__(), "../server/DetectBees/src/camera.jl"))
 
-frame() = binary(collect(vec(cam.Y)))#(Y = cam.Y, u = cam.u, v = cam.v)
+frame!(smallerY, cam) = String(jpeg_encode(rawview(imresize!(smallerY, normedview(cam.Y))); transpose=true))
 
-@get "/frame" frame
+mode = 1
+camera_mode = camera_modes[mode]
+
+const cam = Camera(camera_mode)
+sz = round.(Int, (camera_mode.w, camera_mode.h) ./ 8)
+const smallerY = Matrix{N0f8}(undef, sz)
+const msg = Ref(frame())
+
+fps = FPS(10)
+task = Threads.@spawn while isopen(cam)
+    snap!(cam)
+    msg[] = frame!(smallerY, cam)
+    tick!(fps)
+    yield()
+end
+
+@get "/frame" do
+    msg[]
+end
+
+const ui = """
+<!DOCTYPE html><html>
+<head>
+<meta charset="utf-8" />
+<title>Oxygen App</title>
+</head>
+<body>
+<div>
+<img id="frame">
+</div>
+<script>
+frame = document.querySelector("#frame");
+
+async function loadImage() {
+res = await fetch("/frame");
+imgData = await res.blob();
+frame.src = URL.createObjectURL(imgData);
+}
+
+setInterval(() => {
+loadImage();
+}, 100);
+</script>
+</body>
+</html>
+"""
+
+@get "/" function()
+    return ui
+end
 
 serve(access_log=nothing, host="0.0.0.0", port=8000)
-
