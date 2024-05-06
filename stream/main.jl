447ccb163ea56f764b866adc65be8c88a1944c51
Yakir Gagnon <12.yakir@gmail.com>
2024-05-06 14:49:55 +0200

    wip

--- stream/main.jl
+++ stream/main.jl
@@ -1,7 +1,10 @@
 using Statistics
-using Oxygen, AprilTags
-# import AprilTags.AprilTagDetector
-using ImageCore, StaticArrays, ImageTransformations, JpegTurbo
+using OhMyThreads, AprilTags, StaticArrays, 
+
+const SV = SVector{2, Float64}
+
+# using Oxygen
+# using ImageCore, ImageTransformations, JpegTurbo
 
 struct Detector
     detectors::Channel{AprilTagDetector}
@@ -55,59 +58,59 @@ end
 
 include(joinpath(@__DIR__(), "../server/DetectBees/src/camera.jl"))
 
-frame!(smallerY, cam) = String(jpeg_encode(colorview(Gray, imresize!(smallerY, normedview(cam.Y))); transpose=true))
+# frame!(smallerY, cam) = String(jpeg_encode(colorview(Gray, imresize!(smallerY, normedview(cam.Y))); transpose=true))
 
 mode = 1
 camera_mode = camera_modes[mode]
 
 const cam = Camera(camera_mode)
-sz = round.(Int, (camera_mode.w, camera_mode.h) ./ 8)
-const smallerY = Matrix{N0f8}(undef, sz)
-const msg = Ref(frame!(smallerY, cam))
+# sz = round.(Int, (camera_mode.w, camera_mode.h) ./ 8)
+# const smallerY = Matrix{N0f8}(undef, sz)
+# const msg = Ref(frame!(smallerY, cam))
 
 const detector = Detector(2Threads.nthreads())
 const fps = FPS(50)
 task = Threads.@spawn while isopen(cam)
     snap!(cam)
     detec(detector, cam.Y)
-    msg[] = frame!(smallerY, cam)
+    # msg[] = frame!(smallerY, cam)
     tick!(fps)
     yield()
 end
 
-@get "/frame" function()
-    msg[]
-end
-
-const ui = """
-<!DOCTYPE html><html>
-<head>
-<meta charset="utf-8" />
-<title>Oxygen App</title>
-</head>
-<body>
-<div>
-<img id="frame">
-</div>
-<script>
-frame = document.querySelector("#frame");
-
-async function loadImage() {
-res = await fetch("/frame");
-imgData = await res.blob();
-frame.src = URL.createObjectURL(imgData);
-}
-
-setInterval(() => {
-loadImage();
-}, 100);
-</script>
-</body>
-</html>
-"""
-
-@get "/" function()
-    return ui
-end
-
-serve(access_log=nothing, host="0.0.0.0", port=8000, async=true)
+# @get "/frame" function()
+#     msg[]
+# end
+#
+# const ui = """
+# <!DOCTYPE html><html>
+# <head>
+# <meta charset="utf-8" />
+# <title>Oxygen App</title>
+# </head>
+# <body>
+# <div>
+# <img id="frame">
+# </div>
+# <script>
+# frame = document.querySelector("#frame");
+#
+# async function loadImage() {
+# res = await fetch("/frame");
+# imgData = await res.blob();
+# frame.src = URL.createObjectURL(imgData);
+# }
+#
+# setInterval(() => {
+# loadImage();
+# }, 100);
+# </script>
+# </body>
+# </html>
+# """
+#
+# @get "/" function()
+#     return ui
+# end
+#
+# serve(access_log=nothing, host="0.0.0.0", port=8000, async=true)
