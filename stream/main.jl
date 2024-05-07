9d692e433bb9defe583e164573ada6ffc6edbc95
Yakir Gagnon <12.yakir@gmail.com>
2024-05-07 14:01:30 +0200

    wip

--- stream/main.jl
+++ stream/main.jl
@@ -77,7 +77,7 @@ task = Threads.@spawn while isopen(cam)
 end
 
 using Oxygen, ImageCore, ImageTransformations, JpegTurbo, ImageDraw
-sz = round.(Int, (camera_mode.w, camera_mode.h) ./ 8)
+sz = round.(Int, (camera_mode.w, camera_mode.h) ./ 1)
 const smallerY = Matrix{RGB{N0f8}}(undef, sz)
 mydraw!(_, ::Missing) = nothing
 function mydraw!(img, tag::SVI)
