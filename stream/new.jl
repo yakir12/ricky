39fddc5af4773eabdf323e3428d11b7ed5e31d49
Yakir Gagnon <12.yakir@gmail.com>
2024-05-21 12:00:08 +0200

    wip

--- stream/new.jl
+++ stream/new.jl
@@ -75,20 +75,20 @@ bees = Bee.(0:nbees - 1)
 camera_mode = camera_modes[1]
 
 
-function plot(io, p, xs, ys)
-    show(io, scatterplot!(p, [xs], [ys]))
-    out = read(io, String)
-    REPL.Terminals.clear(terminal)
-    println(out)
-end
-_cursor_hide(io::IO) = print(io, "\x1b[?25l")
-_cursor_show(io::IO) = print(io, "\x1b[?25h")
+# function plot(io, p, xs, ys)
+#     show(io, scatterplot!(p, [xs], [ys]))
+#     out = read(io, String)
+#     REPL.Terminals.clear(terminal)
+#     println(out)
+# end
+# _cursor_hide(io::IO) = print(io, "\x1b[?25l")
+# _cursor_show(io::IO) = print(io, "\x1b[?25h")
 
-terminal = REPL.Terminals.TTYTerminal("", stdin, stdout, stderr)
+# terminal = REPL.Terminals.TTYTerminal("", stdin, stdout, stderr)
 
-_cursor_hide(stdout)
-io = IOContext(PipeBuffer(), :color=>true)
-p = Plot(; xlim=(0, camera_mode.w), ylim=(0, camera_mode.h))
+# _cursor_hide(stdout)
+# io = IOContext(PipeBuffer(), :color=>true)
+# p = Plot(; xlim=(0, camera_mode.w), ylim=(0, camera_mode.h))
 
 
 cam = Camera(camera_mode)
@@ -97,7 +97,8 @@ task1 = Threads.@spawn while isopen(cam)
     tforeach(bees) do bee
         if isalive(bee)
             bee(cam.Y)
-            plot(io, p, bee.center...)
+            println(bee.center)
+            # plot(io, p, bee.center...)
         end
     end
 end
