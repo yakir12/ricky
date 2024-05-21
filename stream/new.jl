f0345ae204dc11a6ff0b66654de43da0665b2c5f
Yakir Gagnon <12.yakir@gmail.com>
2024-05-21 16:06:17 +0200

    wip

--- stream/new.jl
+++ stream/new.jl
@@ -76,20 +76,19 @@ bees = Bee.(0:nbees - 1)
 
 
 
-function plot(io, xs, ys)
-    show(io, scatterplot(xs, ys; xlim=(0, camera_mode.w), ylim=(0, camera_mode.h), width = camera_mode.h ÷ 16, height = camera_mode.w ÷ 16))
-    out = read(io, String)
-    REPL.Terminals.clear(terminal)
-    println(out)
-end
-_cursor_hide(io::IO) = print(io, "\x1b[?25l")
-_cursor_show(io::IO) = print(io, "\x1b[?25h")
-
-terminal = REPL.Terminals.TTYTerminal("", stdin, stdout, stderr)
-
-_cursor_hide(stdout)
-io = IOContext(PipeBuffer(), :color=>true)
-# # p = Plot(; xlim=(0, camera_mode.w), ylim=(0, camera_mode.h))
+# function plot(io, xs, ys)
+#     show(io, scatterplot(xs, ys; xlim=(0, camera_mode.w), ylim=(0, camera_mode.h), width = camera_mode.h ÷ 16, height = camera_mode.w ÷ 16))
+#     out = read(io, String)
+#     REPL.Terminals.clear(terminal)
+#     println(out)
+# end
+# _cursor_hide(io::IO) = print(io, "\x1b[?25l")
+# _cursor_show(io::IO) = print(io, "\x1b[?25h")
+#
+# terminal = REPL.Terminals.TTYTerminal("", stdin, stdout, stderr)
+#
+# _cursor_hide(stdout)
+# io = IOContext(PipeBuffer(), :color=>true)
 
 mutable struct FPS{N}
     i::Int
@@ -117,9 +116,9 @@ task1 = Threads.@spawn while isopen(cam)
             bee(cam.Y)
         end
     end
-    # tick!(fps)
-    points = [bee.center for bee in bees if isalive(bee)]
-    plot(io, first.(points), last.(points))
+    tick!(fps)
+    # points = [bee.center for bee in bees if isalive(bee)]
+    # plot(io, first.(points), last.(points))
 end
 
 task2 = Threads.@spawn while isopen(cam)
