0ad534f70a506a2dd05562c034239d25e9cfd7d2
Yakir Gagnon <12.yakir@gmail.com>
2024-05-21 14:19:54 +0200

    wip

--- stream/new.jl
+++ stream/new.jl
@@ -75,23 +75,38 @@ bees = Bee.(0:nbees - 1)
 camera_mode = camera_modes[1]
 
 
-# function plot(io, p, xs, ys)
-    # show(io, scatterplot!(p, xs, ys))
-function plot(io, xs, ys)
-    show(io, scatterplot(xs, ys; xlim=(0, camera_mode.w), ylim=(0, camera_mode.h)))
-    out = read(io, String)
-    REPL.Terminals.clear(terminal)
-    println(out)
+# function plot(io, xs, ys)
+#     show(io, scatterplot(xs, ys; xlim=(0, camera_mode.w), ylim=(0, camera_mode.h)))
+#     out = read(io, String)
+#     REPL.Terminals.clear(terminal)
+#     println(out)
+# end
+# _cursor_hide(io::IO) = print(io, "\x1b[?25l")
+# _cursor_show(io::IO) = print(io, "\x1b[?25h")
+#
+# terminal = REPL.Terminals.TTYTerminal("", stdin, stdout, stderr)
+#
+# _cursor_hide(stdout)
+# io = IOContext(PipeBuffer(), :color=>true)
+# # p = Plot(; xlim=(0, camera_mode.w), ylim=(0, camera_mode.h))
+
+mutable struct FPS{N}
+    i::Int
+    times::MVector{N, UInt64}
+    FPS{N}() where {N} = new(0, MVector{N, UInt64}(1:N))
 end
-_cursor_hide(io::IO) = print(io, "\x1b[?25l")
-_cursor_show(io::IO) = print(io, "\x1b[?25h")
+FPS(N::Int) = FPS{N}()
 
-terminal = REPL.Terminals.TTYTerminal("", stdin, stdout, stderr)
+function tick!(fps::FPS{N}) where N
+    fps.i += 1
+    fps.times[fps.i] = time_ns()
+    if fps.i == N
+        println(round(Int, 10^9/mean(diff(fps.times))))
+        fps.i = 0
+    end
+end
 
-_cursor_hide(stdout)
-io = IOContext(PipeBuffer(), :color=>true)
-# p = Plot(; xlim=(0, camera_mode.w), ylim=(0, camera_mode.h))
-
+fps = FPS(10)
 
 cam = Camera(camera_mode)
 task1 = Threads.@spawn while isopen(cam)
@@ -101,8 +116,9 @@ task1 = Threads.@spawn while isopen(cam)
             bee(cam.Y)
         end
     end
-    points = [bee.center for bee in bees if isalive(bee)]
-    plot(io, first.(points), last.(points))
+    tick!(fps)
+    # points = [bee.center for bee in bees if isalive(bee)]
+    # plot(io, first.(points), last.(points))
 end
 
 task2 = Threads.@spawn while isopen(cam)
@@ -131,47 +147,6 @@ end
 
 
 
-
-# struct Detector
-#     pool::Channel{AprilTagDetector}
-#     ntags::Int
-#     tile_c
-#     ntasks::Int
-#     function Detector(sz, ndetectors, ntags, ntasks)
-#         pool = Channel{AprilTagDetector}(ndetectors)
-#         foreach(1:ndetectors) do _
-#             put!(pool, AprilTagDetector()) 
-#         end
-#         tiles = TileIterator(Base.OneTo.(sz), (104, 152))
-#         c₀ = [SV(reverse(minimum.(i))) for i in tiles]
-#         tile_c = zip(tiles, c₀)
-#         return new(pool, ntags, tile_c, ntasks)
-#     end
-# end
-#
-# (d::Detector)(img) = tmapreduce(vcat, d.tile_c; ntasks = d.ntasks, scheduler=:greedy) do (tile, c₀)
-#     detector = take!(d.pool)
-#     tags = detector(img[tile...])
-#     put!(d.pool, detector)
-#     [SV(tag.c) + c₀ for tag in tags if tag.id < d.ntags]
-# end
-#
-# mutable struct FPS{N}
-#     i::Int
-#     times::MVector{N, UInt64}
-#     FPS{N}() where {N} = new(0, MVector{N, UInt64}(1:N))
-# end
-# FPS(N::Int) = FPS{N}()
-#
-# function tick!(fps::FPS{N}) where N
-#     fps.i += 1
-#     fps.times[fps.i] = time_ns()
-#     if fps.i == N
-#         println(round(Int, 10^9/mean(diff(fps.times))))
-#         fps.i = 0
-#     end
-# end
-#
 # function plot(io, p, xs, ys)
 #     show(io, scatterplot!(p, xs, ys))
 #     out = read(io, String)
