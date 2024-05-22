2f448bb2877a938583acd4ce8d0308e1261220ac
Yakir Gagnon <12.yakir@gmail.com>
2024-05-22 14:54:24 +0200

    wip

--- stream/show.jl
+++ stream/show.jl
@@ -1,11 +1,26 @@
 using ImageCore, ColorTypes, Sixel, ImageInTerminal
+import REPL
 
 include(joinpath(@__DIR__(), "../server/DetectBees/src/camera.jl"))
 
 cam = Camera(fastest)
 
+function plot(io, img)
+    display(io, collect(colorview(Gray, normedview(img)))[500:600, 500:600])
+    out = read(io, String)
+    REPL.Terminals.clear(terminal)
+    println(out)
+end
+_cursor_hide(io::IO) = print(io, "\x1b[?25l")
+_cursor_show(io::IO) = print(io, "\x1b[?25h")
+
+terminal = REPL.Terminals.TTYTerminal("", stdin, stdout, stderr)
+
+_cursor_hide(stdout)
+io = IOContext(PipeBuffer(), :color=>true)
+
 task = Threads.@spawn while isopen(cam)
     snap!(cam)
-    display(colorview(Gray, normedview(cam.Y))[500:600, 500:600])
+    plot(io, cam.Y)
 end
 
