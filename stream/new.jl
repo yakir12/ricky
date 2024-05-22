65b2d214e3e496741912d8a6a5343271ff0482a3
Yakir Gagnon <12.yakir@gmail.com>
2024-05-22 14:30:03 +0200

    wip

--- stream/new.jl
+++ stream/new.jl
@@ -133,9 +133,9 @@ task1 = Threads.@spawn while isopen(cam)
     end
     # _print(io, count(isalive, bees))
     # tick!(fps)
-    points = [bee.center for bee in bees if isalive(bee)]
-    plot(io, first.(points), last.(points))
-    # plot(io, rotl90(cam.Y))
+    # points = [bee.center for bee in bees if isalive(bee)]
+    # plot(io, first.(points), last.(points))
+    plot(io, rotl90(cam.Y))
 end
 
 task2 = Threads.@spawn while isopen(cam)
