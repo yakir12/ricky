2a1a3d18e91e8fba95e865988466b484c2ddc699
Yakir Gagnon <12.yakir@gmail.com>
2024-05-22 11:28:20 +0200

    wip

--- server/DetectBees/src/camera.jl
+++ server/DetectBees/src/camera.jl
@@ -1,7 +1,7 @@
 # const camera_modes = ((w = 990, h = 1332, fps = 120),
-const camera_modes = ((w = 1332, h = 990, fps = 120.05),
-         (w = 2028, h = 1080, fps = 50.03),
-         (w = 2028, h = 1520, fps = 40.01),
+const camera_modes = ((w = 1332, h = 990, fps = 120.048),
+         (w = 2028, h = 1080, fps = 50.0275),
+         (w = 2028, h = 1520, fps = 40.0096),
          (w = 4056, h = 3040, fps = 10.0))
 
 function get_buffer_img(w, h)
