55fcec24c1797cc01e800ba5ff6e607751a98808
Yakir Gagnon <12.yakir@gmail.com>
2024-05-22 14:59:42 +0200

    wip

--- stream/show.jl
+++ stream/show.jl
@@ -5,22 +5,18 @@ include(joinpath(@__DIR__(), "../server/DetectBees/src/camera.jl"))
 
 cam = Camera(fastest)
 
-function plot(io, img)
-    display(io, collect(colorview(Gray, normedview(img)))[500:600, 500:600])
-    out = read(io, String)
+function plot(img)
     REPL.Terminals.clear(terminal)
-    println(out)
+    display(colorview(Gray, normedview(img))[500:600, 500:600])
 end
-_cursor_hide(io::IO) = print(io, "\x1b[?25l")
-_cursor_show(io::IO) = print(io, "\x1b[?25h")
 
 terminal = REPL.Terminals.TTYTerminal("", stdin, stdout, stderr)
 
+_cursor_hide(io::IO) = print(io, "\x1b[?25l")
 _cursor_hide(stdout)
-io = IOContext(PipeBuffer(), :color=>true)
 
 task = Threads.@spawn while isopen(cam)
     snap!(cam)
-    plot(io, cam.Y)
+    plot(cam.Y)
 end
 
