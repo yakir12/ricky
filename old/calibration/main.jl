a0a901954d4643d0958fbebbc9d1cec1685fa3b8
Yakir Gagnon <12.yakir@gmail.com>
2024-05-23 16:12:50 +0200

    wip

--- old/calibration/main.jl
+++ old/calibration/main.jl
@@ -16,9 +16,14 @@ for θ in 0:π/2:3π/2
     poly!(ax, Circle(to_feeder*Point2f(reverse(sincos(θ))...), feeder_r), color=:gray)
 end
 # poly!(ax, Circle(zero(Point2f), light_r), color=(:yellow, 0.2), strokewidth=0)
-w, h = (40, 35)
-for (i, y) in enumerate(range(-arena_r - 5 - h, step=-h, length=4)), (j, x) in enumerate(range(-1.5w, step=w, length=3))
+w, h = (10.35, 13.92)
+w, h = (32.07, 24.04)
+xs = collect(range(0, step = w, length = ceil(Int, 2arena_r/w)))
+xs .-= xs[end]/2
+ys = collect(range(0, step = h, length = ceil(Int, 2arena_r/h)))
+ys .-= ys[end]/2
+for (i, x) in enumerate(xs), (j, y) in enumerate(ys)
     poly!(ax, Rect(x, y, w, h), color=:transparent, strokewidth=2)
-    text!(ax, x+w/2, y+h/2, text=string(LinearIndices((4, 3))[i, j]), align=(:center, :center))
+    text!(ax, x+w/2, y+h/2, text=string(LinearIndices((length(xs), length(ys)))[i, j]), align=(:center, :center))
 end
 save("toprint.pdf", fig)
