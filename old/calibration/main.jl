d06d3734863f7da0553ee7232c49dd5298b85ba6
Yakir Gagnon <12.yakir@gmail.com>
2024-05-24 13:47:22 +0200

    wip

--- old/calibration/main.jl
+++ old/calibration/main.jl
@@ -19,7 +19,7 @@ end
 # poly!(ax, Circle(zero(Point2f), light_r), color=(:yellow, 0.2), strokewidth=0)
 w, h = (10.35, 13.92)
 w, h = (32.07, 24.04)
-w, h = (2arena_r/5, h/w*2arena_r/5)
+w, h = (2arena_r/4, h/w*2arena_r/5)
 xs = collect(range(0, step = w, length = ceil(Int, 2arena_r/w)))
 xs .-= (xs[end] + w)/2
 ys = collect(range(0, step = h, length = ceil(Int, 2arena_r/h)))
