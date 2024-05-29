32539caa980ba52e648778fe94d40aef9d0d7d09
Yakir Gagnon <12.yakir@gmail.com>
2024-05-29 15:34:09 +0200

    wip

--- server/DetectBees/src/DetectBees.jl
+++ server/DetectBees/src/DetectBees.jl
@@ -27,7 +27,8 @@ end
 function get_pool(ndetectors)
     pool = Channel{AprilTagDetector}(ndetectors)
     foreach(1:ndetectors) do _
-        put!(pool, AprilTagDetector(AprilTags.tagStandard41h12)) 
+        put!(pool, AprilTagDetector()) 
+        # put!(pool, AprilTagDetector(AprilTags.tagStandard41h12)) 
     end
     return pool
 end
